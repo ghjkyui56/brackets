d91e0d094797f33a9f043cbe4cdac4b486c1c5ba
ghjkyui56 <ksushashaborkina@mail.ru>
2020-10-11 22:07:52 +0300

    feat: make task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack=[];
+  let open=0;
+
+  for (let i=0; i<str.length; i++){
+    for (const [x,y] of bracketsConfig){
+      if (str[i] == y) {
+        if (stack[open-1] == x) {
+          stack.pop();
+          open = open - 1; 
+        }
+        else {
+          stack.push(str[i]);
+          open = open + 1;
+        }
+      }
+      else {
+        if (str[i] == x){
+          stack.push(str[i]);
+          open= open + 1;
+        }        
+      }
+    }
+  }
+
+  return !(stack.length);
 }
